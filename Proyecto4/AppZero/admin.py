0d3174f28e97a04107ef4e3f740e414e7b6340a0
Benjamin Roldan <bnjmn93@gmail.com>
2022-04-03 19:44:13 -0500

    Se agrega el superusuario para el panel de administración

--- Proyecto4/AppZero/admin.py
+++ Proyecto4/AppZero/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Institucion)
+
+admin.site.register(Administrativo)
+
+admin.site.register(Docente)
+
+admin.site.register(PersonalLimpieza)
+
